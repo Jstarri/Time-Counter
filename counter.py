3eaf3de7f9f4b32fcf5ad9ea346876c9842af0c7
Ahmar Jenkins <ahmarj519@gmail.com>
2022-12-12 23:50:36 -0500

    Alarm Sound

--- counter.py
+++ counter.py
@@ -1,4 +1,6 @@
 import time
+from playsound import playsound
+from pathlib import Path
 
 def countdown(t):
     while t:
@@ -9,6 +11,7 @@ def countdown(t):
         t -= 1
 
     print('WEE WOO WEE WOO!!')
+    playsound("C:/Users/emper/OneDrive/Documents/GitHub/Time-Counter/alarm.mp3")
 
 # input time in seconds
 t = input("Enter the time in seconds: ")
